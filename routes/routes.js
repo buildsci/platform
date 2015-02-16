821d97d982c5dbd76b2a85001044d6ffd4d83784
nmattise <n.mattise@gmail.com>
2015-02-16 12:03:20 -0500

    new routing try

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ var shell = require('shelljs');
 
 module.exports = {
     getHome: function(request, response){
-        //response.sendfile('./views/home.html');    
+        //response.sendfile('views/home.html');
         response.render('home',{
             'username': request.session.username
         });
@@ -11,48 +11,48 @@ module.exports = {
         response.render('team');  
     },
     getSignUp: function(request, response){
-        response.sendfile('./views/signup.html');  
+        response.sendfile('views/signup.html');
     },
     getLogIn: function(request, response){
-        response.sendfile('./views/login.html');  
+        response.sendfile('views/login.html');
     },
     getLite: function(request, response){
-        response.sendfile('./views/lite.html');    
+        response.sendfile('views/lite.html');
     },
     getLiteConv: function(request, response){
-        response.sendfile('./views/liteconversion.html');    
+        response.sendfile('views/liteconversion.html');
     },
     getLiteResults: function(request, response){
-        response.sendfile('./views/literesults.html');    
+        response.sendfile('views/literesults.html');
     },
     getPartial: function(request, response){
-        response.sendfile('./views/partial.html');    
+        response.sendfile('views/partial.html');
     },
     
     getSubstantial: function(request, response) {
-        response.sendfile('./views/substantial.html');
+        response.sendfile('views/substantial.html');
     },
     
     getComprehensive: function(request, response) {
-        response.sendfile('./views/comprehensive.html');
+        response.sendfile('views/comprehensive.html');
     },
     getNews: function(request, response){
-        response.sendfile('./views/news.html');  
+        response.sendfile('views/news.html');
     }, 
     getVisits: function(request, response){
-        response.sendfile('./views/visitors.html');  
+        response.sendfile('views/visitors.html');
     }, 
     getPresentation: function(request, response){
-        response.sendfile('./views/presentation.html');  
+        response.sendfile('views/presentation.html');
     }, 
     getCaseStudies: function(request, response){
-        response.sendfile('./views/casestudy.html');
+        response.sendfile('views/casestudy.html');
     },
     getEngines: function(request, response){
-        response.sendfile('./views/engine.html');
+        response.sendfile('views/engine.html');
     },
     getAccomplishments: function(request, response){
-        response.sendfile('./views/accomplishments.html');
+        response.sendfile('views/accomplishments.html');
     }
     
 };
